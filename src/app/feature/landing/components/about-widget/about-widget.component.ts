b08e37fe5e0fc7d89a0bdc917a75ff61090c95ac
Boris Sotsky <boris.sotsky@gmail.com>
2021-01-21 13:51:53 +0300

    Small changes

--- src/app/feature/landing/components/about-widget/about-widget.component.ts
+++ src/app/feature/landing/components/about-widget/about-widget.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ContactType } from 'src/app/feature/controls/contact-link/contact.pipe';
 
 @Component({
     selector: 'my-about-widget',
@@ -6,7 +7,10 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutWidgetComponent {
+    public ContactType = ContactType;
+
     public galleryPhotos = ['stencil-on-bench.png', 'drawn-prototypes.png', 'prototypes-notebook.png'].map(photoName => `assets/images/gallery/${photoName}`);
 
     @Input() public cost: string = '29.99$';
+    @Input() public telegram: string;
 }
